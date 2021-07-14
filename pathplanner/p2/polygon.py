cc22a34b4d39dbab5716ca7f08e6c957a9cdc736
Mike Sutherland <msutherl@uci.edu>
2021-07-14 13:44:44 -0700

    working BDC + straightskel centroids

--- pathplanner/p2/polygon.py
+++ pathplanner/p2/polygon.py
@@ -1,21 +1,24 @@
 from datetime import datetime
 from os import PRIO_USER
+from matplotlib import transforms
 from scipy import spatial
 import numpy as np
 import networkx as nx
 from matplotlib import pyplot as plt
-from matplotlib.transforms import offset_copy
+from matplotlib import cm
+import warnings
 
-def cluster_points(no_clusters=3, cluster_n=10, cluster_size=1, cluster_dist=1):
-    ''' generate clusters of points '''
-    pts = np.zeros((no_clusters * cluster_n, 2))
-    loc = np.array([0,0], dtype='float64')
-    for c in range(no_clusters):
-        pts[c * cluster_n:(c+1)* cluster_n, :] = np.random.normal(loc=loc, scale=cluster_size, size=(cluster_n, 2))
-        loc += np.random.uniform(low=-cluster_dist, high=cluster_dist, size=np.shape(loc))
+def beta_clusters(clusters: int=3, ppc: int=20, alpha:float=4.0, beta: float=4.0) -> np.ndarray:
+    pts = np.zeros((clusters*ppc, 2))
+    loc = np.array([0,0], dtype=float)
+    for c in range(clusters):
+        # each cluster is a "block" in the pts array between (c) : (c+1) multiplied by ppc.
+        pts[c*ppc : (c+1)*ppc] = np.random.beta(a=alpha, b=beta, size=(ppc, 2) ) + loc
+        # move center point of cluster
+        loc += np.random.uniform(low=-1, high=1, size=loc.shape)
     return pts
 
-def removable_interiors(dt):
+def removable_interiors(dt: spatial.Delaunay) -> tuple:
     '''find indices of interior simplices that are safe to remove in dt'''
     # all outer tris no exception
     outer_tris = (dt.neighbors == -1).any(axis=1)
@@ -27,7 +30,7 @@ def removable_interiors(dt):
     unsafe, = np.where(s_unsafe.any(axis=1)==True)
     return safe, unsafe
 
-def removable_exteriors(dt, points, ax=None):
+def removable_exteriors(dt: spatial.Delaunay, points: np.ndarray, ax: plt.Axes=None) -> tuple:
     ''' find indices to safe '''
     # set of all edge simplices
     et, = np.where( (dt.neighbors == -1).any(axis=1) == True)
@@ -60,10 +63,9 @@ def removable_exteriors(dt, points, ax=None):
         xyt = points[dt.simplices[et]].sum(axis=1)/3
         for i, e in enumerate(et):
             ax.text(xyt[i,0], xyt[i,1], str(e))
-
     return et_idx.any(), et[et_idx]
 
-def del_tri(dt, rm) -> None:
+def del_tri(dt:spatial.Delaunay, rm: int) -> spatial.Delaunay:
     ''' Alters dt in place to remove tri at `rm` ''' 
     # each neighbor in dt will have -1 where the neighboring simplex used to be.
     for i, nb in enumerate(dt.neighbors):
@@ -82,32 +84,56 @@ def del_tri(dt, rm) -> None:
     dt.neighbors = np.delete(dt.neighbors, rm, axis=0)
     return dt
 
-def ar(M):
+def _dotself(x: np.ndarray) -> np.ndarray:
+    return x.dot(x)
+
+def ar(M: np.ndarray) -> np.float64:
     '''aspect ratio'''
+    # a, b, c are the 3 vectors which form the triangle
     a = M[0,:] - M[1,:]
     b = M[1,:] - M[2,:]
     c = M[2,:] - M[0,:]
-    dotself = lambda x: x.dot(x)
-    a, b, c = map(dotself, (a, b, c))
+    # dot each with self
+    a, b, c = map(_dotself, (a, b, c))
+    # half chord
     s = (a+b+c)/2.0
-    return (a*b*c/(8.0*(s-a)*(s-b)*(s-c))).mean()
+    # https://codegolf.stackexchange.com/questions/101234/evaluate-the-aspect-ratio-of-a-triangle
+    ar = (a*b*c/(8.0*(s-a)*(s-b)*(s-c))).mean()
+    return ar
 
 
-def polygon(points: np.array, holes: int, removals=30) -> nx.DiGraph():
+def polygon(points: np.ndarray, holes: int = 0, removals: int = 30) -> nx.DiGraph():
+    '''Create a non-convex polygon from points. This algorithm works by first creating a convex
+    Delaunay triangulation over the points, and then removing triangles to form a non-convex 
+    polygon. Thus, the general shape of the initial point distribution will infrom the subsequent
+    non-convex polygon.
+
+    This algorithm is slow for large numbers of points.
+
+    Parameters
+    ----------
+    points : np.ndarray, holes, optional
+        The number of interior holes in the polygon, default 0
+
+    Returns
+    -------
+    nx.Digraph
+        Graph containing the polygon.
+    '''
     dt = spatial.Delaunay(points)
-    # holes
-    for i in range(holes):
+    # remove holes
+    for _ in range(holes):
         safe, _ = removable_interiors(dt)
         if safe.size > 0:
             dt = del_tri(dt, np.random.choice(safe))
-    # exteriors
+    # remove exterior tris
     n=0
     while removable_exteriors(dt, points)[0] and n < removals:
         n+=1
         _, ets = removable_exteriors(dt, points)
         ets = sorted(ets, key=lambda x: ar(points[dt.simplices[x]]))
         dt = del_tri(dt, np.random.choice(ets))
-
+    # traverse and find outer edges
     outers=[]
     for s, n in zip(dt.simplices, dt.neighbors):
         if (n==-1).any():
@@ -124,16 +150,20 @@ def polygon(points: np.array, holes: int, removals=30) -> nx.DiGraph():
             # not possible
             else:
                 raise(Exception('Tri identified as outside that is not on the outside!'))
- 
+    # first we make an undirected graph, then we create the directed graph by traversing the 
+    # undirected in a DFS.
     G = nx.Graph()
     G.add_edges_from(outers)
     H = nx.DiGraph()
     H.add_edges_from(list(nx.edge_dfs(G)))
+    # store point coordinates into each node
     for n in H.nodes:
         H.nodes[n]['points'] = points[n]
-
+    # leftmost point is guaranteed to be on the outer boundary, therefore any node connected to it
+    # is also on the outer boundary (and any node not connected to it is on an interior boundary.)
     leftpt = sorted(H.nodes, key = lambda n: points[n][0])
-    # for each loop 
+    # we are going to break the whole graph down into subgraphs (which are simple cycles) so that we
+    # can orient each cycle depending on whether it is on the outside or not
     outputgraphs = []
     for cyc in nx.simple_cycles(H):
         M = H.subgraph(cyc).copy()
@@ -143,13 +173,16 @@ def polygon(points: np.array, holes: int, removals=30) -> nx.DiGraph():
             # set both nodes and edges
             for (e1, e2), c in zip(M.edges, cyc):
                 outer = True
+                M[e1][e2]['weight'] = 1
                 cw += addcw(H, e1, e2)
         else:
             for (e1, e2), c in zip(M.edges, cyc):
+                M[e1][e2]['weight'] = 2
                 outer = False
                 cw += addcw(H, e1, e2)
-        cw = cw >= 0
-        M = H.subgraph(cyc).copy()
+        cw = cw >= 0        
+        # categorize nodes
+        # append
         if cw and outer:
             outputgraphs.append(M)
         elif not cw and outer:
@@ -158,45 +191,54 @@ def polygon(points: np.array, holes: int, removals=30) -> nx.DiGraph():
             outputgraphs.append(M)
         elif cw and not outer:
             outputgraphs.append(nx.reverse(M, copy=True))
-    return nx.compose_all(outputgraphs)
+    out_graph = nx.compose_all(outputgraphs)
+    return out_graph
     
-def addcw(H, e1, e2):
+def addcw(H: nx.DiGraph, e1: int, e2: int) -> float:
+    '''determine which way the edge is pointing
+             Q2  │  Q1
+            ─────┼─────
+             Q3  │  Q4
+    A "positive" cw value goes from Q2 to Q4. A "negative"
+    cw value means the edge goes from Q4 to Q2. It's relative
+    so we only look at directions not absolute positions.
+    '''
     p1, p2 = H.nodes[e1]['points'], H.nodes[e2]['points']
     return (p2[0] - p1[0]) * (p2[1] + p1[1])
 
-
-def draw_G(G, ax, posattr='points'):
-    pos = nx.get_node_attributes(G, 'points')
+def draw_G(
+    G: nx.DiGraph, 
+    ax: plt.Axes, 
+    posattr: str ='points', 
+    arrows: bool =False, 
+    nodecolor: str = 'k', 
+    ecolor: str = None,
+    ecolorattr: str = 'weight',
+    node_text: bool = True,
+    style: str = '-',
+    m: str = '.',
+    draw_nodes = True,
+    ) -> plt.Axes:
+    '''Draw a DiGraph `G` with points stored in `posattr` onto `ax`'''
+    pos = nx.get_node_attributes(G, posattr)
+    if not ecolor:
+        try:
+            ecolor = [G[u][v][ecolorattr] for u, v in G.edges()]
+        except:
+            ecolor = [5 for _ in G.edges()]
     nx.draw_networkx_edges(G, pos, ax=ax,
-        node_size=4)
-    nx.draw_networkx_nodes(
-        G, pos, ax=ax,
-        node_shape='.',
-        node_color='k',
-        node_size=30,)
+        node_size=4,
+        arrows=arrows,
+        edge_color=ecolor,
+        style=style,
+        edge_cmap=plt.get_cmap('tab10'))
+    if draw_nodes:
+        nx.draw_networkx_nodes(
+            G, pos, ax=ax,
+            node_shape=m,
+            node_color=nodecolor,
+            node_size=30,)
+    if node_text:
+        for n in G.nodes: ax.text(pos[n][0], pos[n][1], s=str(n))
     ax.autoscale(tight=False)
     return ax
-
-def chart(points, G, ax, cm=plt.cm.tab10):
-    '''
-    Plot the graph `G` on `ax`
-    '''
-    pos = {}
-    for n in G.nodes:
-        pos[n] = points[n]
-    edges = G.edges
-    nx.draw(G, pos, node_size=16, ax=ax, edgelist=edges, edge_cmap=cm)
-    ax.set_aspect('equal')
-    def offset(ax, x, y):
-        return offset_copy(ax.transData, x=x, y=y, units='dots')
-    for n in G.nodes:
-        x, y = points[n][0], points[n][1]
-        ax.text(x, y, str(n), fontsize=9, transform=offset(ax, 0, 5))
-
-if __name__ == '__main__':
-    points = np.random.poisson(lam=100, size=(180, 2))
-    points = np.random.normal(size=(180, 2))
-    G = polygon(points, 8)
-    fig, ax = plt.subplots()
-    draw_G(G, ax)
-    plt.show()
