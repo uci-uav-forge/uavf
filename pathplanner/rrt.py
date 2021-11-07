7d36d41124b2793dcc60ef3f354de67aa275ca8a
Mike Sutherland <msutherl@uci.edu>
2021-11-06 21:17:25 -0700

    RRT does not work atm

--- pathplanner/rrt.py
+++ pathplanner/rrt.py
@@ -3,8 +3,35 @@ import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 import matplotlib.pyplot as plt
 from mayavi import mlab
-from tvtk.util import ctf
 from matplotlib import cm
+import rrtpp
+from tqdm import tqdm
+from scipy.spatial.distance import euclidean
+
+
+class SheetRRT(rrtpp.RRTStar):
+    def __init__(self, wshape, X, Y, H, alpha):
+        # world is blank by default
+        world = np.zeros(wshape)
+        super().__init__(world)
+        self.H = H
+        self.alpha = alpha
+        self.interp = self.make_interp(X, Y, H)
+
+    def make_interp(self, X, Y, H):
+        """make a linear interpolator"""
+        x = X[0, :]
+        y = Y[:, 0]
+        return RegularGridInterpolator((x, y), H)
+
+    def calc_cost(self, v, x=None):
+        print(x)
+        r2distcost = euclidean(self.T.nodes[v]["point"], x)
+        if x is not None:
+            cost = self.T.nodes[v]["cost"] + r2distcost
+        else:
+            cost = self.T.nodes[v]["cost"]
+        return cost
 
 
 def xform_2dline(p1, p2, X, Y, H):
@@ -16,27 +43,15 @@ def xform_2dline(p1, p2, X, Y, H):
     return line2d, lineh
 
 
-def cmap_to_ctf(cmap_name):
-    values = list(np.linspace(0, 1, 256))
-    cmap = cm.get_cmap(cmap_name)(values)
-    transfer_function = ctf.ColorTransferFunction()
-    for i, v in enumerate(values):
-        transfer_function.add_rgb_point(v, cmap[i, 0], cmap[i, 1], cmap[i, 2])
-    return transfer_function
-
-
 @mlab.show
 def plot_mayavi(X, Y, H, Hnew):
-
-    s1 = mlab.surf(X, Y, Hnew, opacity=0.5, colormap="greys")
-    s1 = mlab.surf(X, Y, H, colormap="gist_earth")
-    # signal for update
-    s1.update_ctf = True
+    s1 = mlab.mesh(X, Y, Hnew, opacity=0.5, colormap="Greys")
+    s2 = mlab.mesh(X, Y, H, colormap="terrain")
 
 
 if __name__ == "__main__":
-    min_acc_climb = 0.25
-    min_climb = 4
+    min_acc_climb = 0.5
+    min_climb = 2
     # minimum distance
     buffer = 1
     min_altitude = 10
@@ -44,8 +59,8 @@ if __name__ == "__main__":
     astar_heightcost = 0.001
 
     xrange, yrange = (0, 30), (0, 30)
-    radrange = (2, 9)
-    hrange = (1, 50)
+    radrange = (3, 6)
+    hrange = (1, 25)
 
     X, Y = optimal_path.generate_xy_grid(xrange, yrange, 1)
     obstacles = optimal_path.generate_obstacles(4, xrange, yrange, radrange, hrange)
@@ -53,7 +68,13 @@ if __name__ == "__main__":
     Hnew = optimal_path.get_optimal_grid(
         H, buffer, min_climb, min_acc_climb, min_altitude
     )
-    plot_mayavi(X, Y, H, Hnew)
+
+    rrt = SheetRRT(X.shape, X, Y, H, 1.0)
+    end = np.array([X[2, 2], Y[2, 2]])
+    start = np.array([X[-2, -2], Y[-2, -2]])
+
+    rrt.make(start, end, 100, 10)
+
     """
     # plot
     fig = plt.figure(figsize=(8, 4.5), dpi=120, tight_layout=True)
