1905f12f935009f89ab3b901dd6aa1e226f7b440
Mike Sutherland <msutherl@uci.edu>
2021-11-11 11:03:08 -0800

    removed demo code in astar.py

--- pathplanner/p2/astar.py
+++ pathplanner/p2/astar.py
@@ -68,7 +68,7 @@ def reconstruct_path(predecessors, current):
     while current in predecessors:
         current = predecessors[current]
         total.append(current)
-    return total
+    return total[::-1]
 
 
 def get_line(X, Y, H, path, dims=2, c="k"):
@@ -83,39 +83,3 @@ def get_line(X, Y, H, path, dims=2, c="k"):
             xy = xyh[i, j, :]
             lc.append(xy)
     return np.array(lc)
-
-
-if __name__ == "__main__":
-    import optimal_path
-    from matplotlib import pyplot as plt
-    from matplotlib import cm
-
-    xrange, yrange, step = (0, 50), (0, 50), 1
-    X, Y = optimal_path.generate_xy_grid(xrange, yrange, step)
-    obstacles = optimal_path.generate_obstacles(3, xrange, yrange, (5, 12), (2, 8))
-    Hground = optimal_path.place_obstacles(X, Y, obstacles)
-    dh, d2h, buffer, min_h = 0.4, 0.05, 1.0, 1.5
-    Hsheet = optimal_path.get_optimal_grid(Hground, buffer, dh, d2h, min_h)
-    # plot 2d
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot()
-    optimal_path.plot_mpl_2d(ax1, X, Y, Hsheet)
-
-    # plot 3d
-    fig2 = plt.figure(tight_layout=True)
-    ax2 = fig2.add_subplot(projection="3d")
-    optimal_path.plot_mpl3d(ax2, X, Y, Hground, Hsheet, wireframe=True)
-
-    start = 1, 1
-    goal = (X.shape[0] - 1, X.shape[1] - 1)
-
-    heurs = ("euclideanR3", "least_diff", "lowest", "euclideanR2")
-    heur_cost = [0, 1.0, 2.0, 4.0, 8.0, 10.0]
-
-    for i, hc in enumerate(heur_cost):
-        print(i)
-        path = astar(X, Y, Hsheet, start, goal, hcost=hc)
-        lc = get_line(X, Y, Hsheet, path)
-        ax1.plot(lc[:, 0], lc[:, 1], label="H Cost={}".format(hc))
-    ax1.legend()
-    plt.show()
