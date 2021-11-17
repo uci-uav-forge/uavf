0781b9f92107c643f549c8e8c60190c48be35d4f
rland93 <msutherland@fastmail.com>
2021-11-17 13:38:27 -0800

    fixed sphinx theme cutting off module class / function descriptions in summary

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,7 @@ sys.path.insert(0, os.path.abspath(".."))
 try:
     import odcl
 except ImportError:
-    raise ImportError(
-        "check that `odcl` is available to your system path."
-    )
+    raise ImportError("check that `odcl` is available to your system path.")
 
 # -- Project information -----------------------------------------------------
 
@@ -66,10 +64,8 @@ autodoc_mock_imports = ["scipy", "numpy", "matplotlib"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pydata_sphinx_theme"
 
-html_theme_options = {"sidebarwidth": "150"}
-
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
