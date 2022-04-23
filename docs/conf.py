81e5db3bc976b6747572e34f8fd6d3c092ac59eb
Mike Sutherland <msutherl@uci.edu>
2022-04-23 12:13:40 -0700

    fixed docs not building #23

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../src"))
 try:
     import odcl
 except ImportError:
@@ -44,7 +44,7 @@ extensions = [
 ]
 
 autoapi_type = "python"
-autoapi_dirs = ["../odcl/"]
+autoapi_dirs = ["../src/"]
 
 
 # Add any paths that contain templates here, relative to this directory.
