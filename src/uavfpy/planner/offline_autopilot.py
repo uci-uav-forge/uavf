689e2f04ba81124344efa20f2b5656bcd7cf17ad
Epicrider <omarepic1@gmail.com>
2022-05-22 02:23:22 -0700

    Partial Mission Path Planner Integration with OfflinePlanner

--- src/uavfpy/planner/offline_autopilot.py
+++ src/uavfpy/planner/offline_autopilot.py
@@ -2,16 +2,34 @@
 # offline_autopilot.py
 
 
-from uavfpy.planner.mission import Mission
+from uavfpy.planner.mission import Mission, get_xformer_from_CRS_str
 
 
 class OfflinePlanner:
 
-    m = None
+    # Mission Object
+    mission = None
+
 
     def __init__(self, fileName, interopName):
         self.f = open(fileName + ".waypoints", "x")
-        # self.m = Mission(open(interopName).read(), )
+        with open(interopName) as f:
+            mission_json = f.read()
+        wgs2loc = get_xformer_from_CRS_str("WGS84", "Irvine")
+        loc2wgs = get_xformer_from_CRS_str("Irvine", "WGS84")
+        self.mission = Mission(
+            mission_json, 
+            wgs2loc, 
+            loc2wgs, 
+            grid_buffer=(2.0, 2.0),
+            grid_max_npoints=(200, 200),
+            grid_stepxy=(5.0, 5.0),
+        )
+        self.mission.solve_Hsurf(
+            buffer=10.0, max_dh=8.0, max_d2h=0.06, solve_shape=(60, 60), verbose=False
+        )
+        self.mission.compute_plan_thru_waypoints
+        waypointpath = self.mission.compute_plan_thru_waypoints(mission.waypoints, n=400)
         self.set_beginning()
 
     def __init__(self, fileName):
